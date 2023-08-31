bfec9f1a48ad09971bb79292af8bccfb7719c45d
Md: Redoan Ahmmad <92013878+rayhan360@users.noreply.github.com>
2023-08-31 23:43:46 +0600

    showing category section buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('javascript file loaded');
+const manageCategory = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await response.json();
+    const phTube = data.data;
+    // console.log(tube);
+
+    const categoryContainer = document.getElementById('tab-container');
+
+    phTube.forEach((category) => {
+        const createDiv  = document.createElement('div');
+        createDiv.innerHTML = `
+        <a class="tab bg-gray-300 text-gray-600 rounded-lg">${category.category}</a>
+        `;
+        categoryContainer.appendChild(createDiv)
+    });
+    
+}
+
+manageCategory()
